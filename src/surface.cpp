f8db486b0ae4d27e97918789e838240335208081
Lt-Henry <quiqueiii@gmail.com>
2018-03-18 23:22:09 +0100

    Implementing memory surfaces

--- src/surface.cpp
+++ src/surface.cpp
@@ -14,7 +14,33 @@ Surface::Surface()
     data=nullptr;
 }
 
+Surface::Surface(int width,int height)
+{
+    this->width=width;
+    this->height=height;
+    
+    bpp = 4;
+    pitch = width*bpp;
+    
+    data = new uint8_t[width*height*bpp];
+}
+
 Surface::~Surface()
 {
 }
 
+void Surface::destroy()
+{
+    delete [] data;
+    
+    data = nullptr;
+}
+
+void Surface::fill(uint32_t pixel)
+{
+
+}
+
+void Surface::blit(Surface& surface,int x,int y)
+{
+}
--- src/surface.hpp
+++ src/surface.hpp
@@ -11,14 +11,50 @@ namespace ds
     {
         public:
         
+        /*! width in pixels */
         uint32_t width;
-        uint32_t height;
-        uint32_t bpp;
-        uint32_t pitch;
-        void* data;
         
+        /*! height in pixels */
+        uint32_t height;
+        
+        /*! bytes per pixel */
+        uint32_t bpp;
+        
+        /*! pitch in bytes */
+        uint32_t pitch;
+        
+        /*! byte data */
+        uint8_t* data;
+        
+        /*!
+            Default constructor, does not create the surface
+        */
         Surface();
+        
+        /*!
+            Creates a surface with given size
+        */
+        Surface(int width,int height);
+        
+        /*!
+            Default destructor, surface data is not freed here
+        */
         virtual ~Surface();
+        
+        /*!
+            Destroys surface content
+        */
+        virtual void destroy();
+        
+        /*!
+            Fills surface with given pixel
+        */
+        void fill(uint32_t pixel);
+        
+        /*!
+            Blits another surface at x,y coordinates
+        */
+        void blit(Surface& surface,int x,int y);
     };
 }
 
